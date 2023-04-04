1ed53aa0f7e9dbf436c87230e077ff6230af5149
ReiRyuSTI <ry-tanaka@sios.com>
2023-04-04 15:19:46 +0900

    initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { RecoilRoot } from 'recoil';
-
 import './App.css';
-import { RouterConfig } from './router/RouterConfig';
-import { AzureConfigComponent } from './utilities/AzureConfigComponent';
 
 function App() {
-  return (
-    <AzureConfigComponent>
-      <RecoilRoot>
-        <RouterConfig />
-      </RecoilRoot>
-    </AzureConfigComponent>
-  );
+  return <></>;
 }
 
 export default App;
