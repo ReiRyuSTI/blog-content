12c4b69e0a538f3da8a4fcd138f5726a0ce291e9
ReiRyuSTI <ry-tanaka@sios.com>
2023-04-06 14:41:32 +0900

    point check

--- src/components/pages/HookFormPage.tsx
+++ src/components/pages/HookFormPage.tsx
@@ -1,13 +1,11 @@
 import { HookFormTest0 } from '../modules/HookForm0';
-import { HookFormTest1 } from '../modules/HookForm1';
-import { HookFormTest2 } from '../modules/HookForm2';
 
 export const HookFormPage = () => {
   return (
     <main className="flex flex-col gap-20">
       <HookFormTest0 />
-      <HookFormTest1 />
-      <HookFormTest2 />
+      {/* <HookFormTest1 />
+      <HookFormTest2 /> */}
     </main>
   );
 };
